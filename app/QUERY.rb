1ce07820023c2c7d3c81a28ee555ac84532e1219
mr.danielcosta <31771918+danielhrc@users.noreply.github.com>
2020-02-19 21:11:29 -0300

    Update QUERY.rb

--- app/QUERY.rb
+++ app/QUERY.rb
@@ -1,1 +1,11 @@
-QUERY="query { viewer { login }}"
+QUERY="query { 
+	search(query:\"stars:>100\",type:REPOSITORY,first:10){
+    	nodes{
+        ... on Repository{
+          nameWithOwner 
+          url
+          createdAt
+        }
+      }
+  }
+}"
