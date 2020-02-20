5b427f7c8019687b0d3ab7070e1b547b21ba1e26
mr.danielcosta <31771918+danielhrc@users.noreply.github.com>
2020-02-20 20:17:58 -0300

    Update QUERY.rb

--- app/QUERY.rb
+++ app/QUERY.rb
@@ -1,11 +1,28 @@
 QUERY="query { 
-	search(query:\"stars:>100\",type:REPOSITORY,first:10){
-    	nodes{
-        ... on Repository{
-          nameWithOwner 
-          url
-          createdAt
-        }
-      }
-  }
-}"
+	search(query: "stars:>100", type: REPOSITORY, first: 100) {
+    	nodes {
+   		   ... on Repository {
+      			  nameWithOwner
+      			  url
+    			  createdAt
+        		  updatedAt
+        		  pullRequests(states: MERGED) {
+          			totalCount
+        		   }
+       			  releases {
+         			totalCount
+       			   }
+       			   primaryLanguage{
+          			name
+       			   }
+       			   numeroTotalIssues: issues {
+       			        totalCount
+       			   }
+        		   numeroTotalIssuesClosed: issues(states: CLOSED) {
+         			totalCount
+       			   }
+      		    }
+   		}
+  	}
+	
+      }"
