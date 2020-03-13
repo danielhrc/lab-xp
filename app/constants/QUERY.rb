8b9969abc579891489b24991d1cc58089109ca58
mr.danielcosta <31771918+danielhrc@users.noreply.github.com>
2020-03-12 22:48:51 -0300

    Update QUERY.rb

--- app/constants/QUERY.rb
+++ app/constants/QUERY.rb
@@ -1,33 +1,34 @@
 
 QUERY= "query{
-        search(query:\"stars:>100\", type:REPOSITORY, first:20){
-    				pageInfo{
-                hasNextPage
-                endCursor
-            }
-   				 nodes{
-                ... on Repository{
-                    nameWithOwner
-                    url
-                    createdAt
-                    pullRequests(states:MERGED){
-                        totalCount
-                    }
-                    updatedAt
-                    releases {
-                        totalCount
-                    }
-                    opened_issues: issues{
-                        totalCount
-                    }
-                    closed_issues: issues(states:CLOSED){
-                       totalCount
-                   }
-                }
-            }
+  search(query: \"stars:>100\", type: REPOSITORY, first: 1){
+    pageInfo {
+      hasNextPage
+      endCursor
+    }
 
+    nodes {
+      ... on Repository {
+        nameWithOwner
+        url
+        createdAt
+        pullRequests(states: MERGED) {
+          totalCount
         }
-
-
-      }    "
+        updatedAt
+        releases {
+          totalCount
+        }
+        opened_issues: issues {
+          totalCount
+        }
+        closed_issues: issues(states: CLOSED) {
+          totalCount
+        }
+        primaryLanguage {
+          name
+        }
+      }
+    }
+  }
+} "
 
